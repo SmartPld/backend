859cf5e178a1a44f11bb952f1fcf846b49eb26e5
Brindesable <ollivier.kilian@gmail.com>
2016-04-26 16:22:01 +0000

    Connexion bdd

--- common/models/station.js
+++ common/models/station.js
@@ -1,13 +1,3 @@
 module.exports = function(Station) {
-
-    var mariadb = ;
-
-    mariadb.createModel(Station.name, Station.properties, Station.options);
-
-    mariadb.automigrate(function () {
-        mariadb.discoverModelProperties('CUSTOMER_TEST', function (err, props) {
-            console.log(props);
-        });
-    });
-
+    
 };
